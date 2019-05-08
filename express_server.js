c5dbbd7fd471be6a5137a321def3ead18188d878
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-08 04:21:10 +0000

    updating project files

--- express_server.js
+++ express_server.js
@@ -4,12 +4,17 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+}
+
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello!");
@@ -31,10 +36,19 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     //let templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };  
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
